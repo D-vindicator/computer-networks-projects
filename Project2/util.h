22038eded65f38f4f0cc8c25d6fe3dc34a8a9916
Derrick <lizhengrong920211@gmail.com>
2015-10-28 00:07:24 -0400

    project 2 started

---
+++ Project2/util.h
@@ -0,0 +1,30 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <unordered_map>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
